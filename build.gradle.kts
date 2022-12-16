d5bd9849ff5bf3ce0041196fe772611da3f60f69
Andrii Vodvud <dxpn37@motorolasolutions.com>
2022-12-16 23:48:32 +0200

    Configured OpenAPI documentation.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.6.7"
-	id("io.spring.dependency-management") version "1.1.0"
-	kotlin("jvm") version "1.7.21"
-	kotlin("plugin.spring") version "1.7.21"
-	id("org.openapi.generator") version "6.2.1"
+    id("org.springframework.boot") version "2.6.7"
+    id("io.spring.dependency-management") version "1.1.0"
+    kotlin("jvm") version "1.7.21"
+    kotlin("plugin.spring") version "1.7.21"
+    id("org.openapi.generator") version "6.2.1"
 }
 
 group = "com.tui"
@@ -13,52 +13,65 @@ version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
 configurations {
-	compileOnly {
-		extendsFrom(configurations.annotationProcessor.get())
-	}
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
 }
 
 repositories {
-	mavenCentral()
+    mavenLocal()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-hateoas")
-	implementation("org.springframework.boot:spring-boot-starter-security")
-		implementation("org.springframework.boot:spring-boot-starter-data-rest")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.springframework.boot:spring-boot-starter-cache")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.springframework.boot:spring-boot-starter-hateoas")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-data-rest")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-cache")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
-	//cache
-	implementation("com.github.ben-manes.caffeine:caffeine:3.0.5")
+    //cache
+    implementation("com.github.ben-manes.caffeine:caffeine:3.0.5")
 
-	//swagger
-	implementation("io.springfox:springfox-boot-starter:3.0.0")
-	implementation("io.springfox:springfox-swagger2:3.0.0")
-	implementation("io.springfox:springfox-swagger-ui:3.0.0")
-	implementation("io.springfox:springfox-bean-validators:3.0.0")
+    //swagger
+    implementation("io.springfox:springfox-boot-starter:3.0.0")
+    implementation("io.springfox:springfox-swagger2:3.0.0")
+    implementation("io.springfox:springfox-swagger-ui:3.0.0")
+    implementation("io.springfox:springfox-bean-validators:3.0.0")
 
-	//github
-	implementation("org.kohsuke:github-api:1.313")
+    //github
+    implementation("org.kohsuke:github-api:1.313")
 
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.springframework.security:spring-security-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.security:spring-security-test")
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "17"
-	}
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "17"
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
 
 tasks.bootJar {
-	archiveFileName.set("vcs-repository-search.jar")
+    archiveFileName.set("vcs-repository-search.jar")
+}
+
+tasks.openApiGenerate {
+    generatorName.set("spring")
+    generatorName.set("kotlin")
+    apiPackage.set("com.tui.pet.api")
+    modelPackage.set("com.tui.pet.model")
+    configOptions.put("delegatePattern", "false")
+    inputSpec.set("$rootDir/src/main/resources/repository-search.yml")
+    outputDir.set("$buildDir/generated")
+    importMappings.put("Pageable", "org.springframework.data.domain.Pageable")
+    importMappings.put("PagedModel", "org.springframework.hateoas.PagedModel")
 }
